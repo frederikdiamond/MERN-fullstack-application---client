23b74e5cfcaf23d06f180a3665ada16317ce94e5
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-01-01 17:40:35 +0100

    Testing to see if it works correctly.

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+import RandomTest from "scenes/screenTest";
+
+const HomePage = () => {
+    return (
+        <Box>
+            <RandomTest />
+            {/* <Navbar /> */}
+        </Box>
+    );
+};
+
+export default HomePage;
